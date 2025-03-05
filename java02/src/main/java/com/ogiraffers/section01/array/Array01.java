d5de31827c7b2373616174ce60b19aabba1ecd81
chk-jk <hosick3130@gmail.com>
2025-03-05 17:43:05 +0900

    minor update(Array01)

--- java02/src/main/java/com/ogiraffers/section01/array/Array01.java
+++ java02/src/main/java/com/ogiraffers/section01/array/Array01.java
@@ -17,17 +17,19 @@ public class Array01 {
                 System.out.println();
             }
         }
+        System.out.println("배열의 주소는 : " + arr.toString() +" 입니다.");
         int avg = getAvg(arr); // 배열의 평균을 계산하는 스태틱 메서드를 통해 평균 값을 구함
-        System.out.println("배열의 평균은 "+ avg +"입니다.");
-        System.out.print("변경된 배열에 저장된 값은 : ");
+        System.out.println("배열의 평균은 "+ avg +" 입니다.");
+        System.out.print("현재 배열에 변경된 값은 : ");
         for (int i=0; i<arr.length; i++) { // 인덱스를 이용해 같은 주소의 배열에 랜덤하게 새 값을 저장
             arr[i] = (int)(Math.random() * 100) + 1;
             System.out.print(arr[i]+" ");
 
             if(i==arr.length-1) {
-                System.out.print("입니다");
+                System.out.println("입니다");
             }
         }
+        System.out.println("배열의 주소는  " + arr.toString()+ " 으로 동일합니다.");
     }
     public static int getAvg(int[] array) { // 배열의 평균을 구하는 스태틱메소드
 
