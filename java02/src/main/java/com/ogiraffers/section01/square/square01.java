054ddf5b92f4a33d0d60610d8f5e25423209f220
chk-jk <hosick3130@gmail.com>
2025-03-05 16:55:06 +0900

    직사각형 넓이 구하기

---
+++ java02/src/main/java/com/ogiraffers/section01/square/square01.java
@@ -0,0 +1,17 @@
+package com.ogiraffers.section01.square;
+
+import java.util.Scanner;
+
+public class square01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("직사각형의 가로 길이를 입력해주세요 : ");
+        double width = sc.nextDouble();
+        System.out.println("직사각형의 세로 길이를 입력해주세요 : ");
+        double length = sc.nextDouble();
+        double extent = length*width;
+
+        System.out.println("직사각형의 넓이는 : " + extent + "입니다.");
+    }
+}
